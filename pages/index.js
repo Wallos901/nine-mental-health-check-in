a7daa28d7634a596d505ee114ddd3160b55f585f
Ben Walton <ben.wallos901@gmail.com>
2023-02-13 08:42:58 +1100

    Project - Set up the initial project.

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout/Layout";
+
+const Home = () => {
+    return (
+        <Layout
+            title="Nine Mental Health Check In"
+            description="Discover the ins and outs of working at Nine with this fun and interactive learning tool."
+        />
+    );
+};
+
+export default Home;
